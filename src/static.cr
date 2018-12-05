f8304cb79ee12e4d15b71625a8e359946be04b17
Kyle Pritchard <kylepritchard@hotmail.co.uk>
2018-12-05 17:11:09 +0100

    Implement glob for files and start building data prior to rendering

--- src/static.cr
+++ src/static.cr
@@ -1,28 +1,5 @@
-# Comment
-# I think we now have some syncing
+require "./static/*"
 
-require "front_matter"
-
-
-File.open("files/test.md", "r") { |fd|
-    FrontMatter.parse(fd) { |front_matter, content_io|
-    
-        puts front_matter
-        puts content_io
-
-		# Do something with the front matter and content.
-		# Parse the front matter as YAML, JSON or something else?
-
-	}
-}
-
-FrontMatter.open("files/test.md") { |front_matter, content_io|
-
-	# Do something with the front matter and content.
-	# Parse the front matter as YAML, JSON or something else?
-
-    puts front_matter
-    puts content_io.gets_to_end
-
-    
-}
+blog = Static::Builder.new("files")
+blog.process_files
+# blog.print_data
